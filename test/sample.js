049766c261a61d6a121be2ebe5b0f09b4ad4eb76
Jake Januszanis <jakejanuszanis@Jakes-MacBook-Air.local>
2026-08-05 20:16:03 -0700

    Adding code sample to test ai review.

---
+++ test/sample.js
@@ -0,0 +1,18 @@
+
+
+
+function filterAndSort(data) {
+    let filtered = [];
+    let index = 0;
+    
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].active) {
+            filtered[index] = data[i];
+            index++;
+        }
+    }
+    
+    filtered.sort((a, b) => b.score - a.score);
+    
+    if (filtered) return filtered;
+}
